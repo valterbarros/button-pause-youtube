70cf8586cdd43c4d2c65ac49b4e027d57de650a7
Marcos Garcia de Lima <marckfree@gmail.com>
2017-10-09 13:55:33 -0300

    Add babel, eslint, es6
    * add tasks:
    
     - npm run js-lint -- lint js files using eslint
     - npm run js-compile -- lint and compile es6 js files to compatible js files using babel

--- js/popup/popup.js
+++ js/popup/popup.js
@@ -1,25 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-  chrome.tabs.query({},function(tabs){     
-    var select = document.getElementById("selectButton");
-    var option;
-    for (var i = 0; i < tabs.length; i++) {
-      if(chrome.extension.getBackgroundPage().window.list_config.checkYoutubeSite(tabs[i].url)){
-        option = document.createElement("option");
-        option.text = tabs[i].title;
-        option.value = tabs[i].id;
-        select.add(option);
-        if(chrome.extension.getBackgroundPage().window.list_config.getCurrentTab() == option.value){
-          option.selected = "selected";
+document.addEventListener('DOMContentLoaded', () => {
+
+  chrome.tabs.query({}, tabs => {
+    const select = document.getElementById('selectButton')
+
+    let option
+
+    for (let i = 0, len = tabs.length; i < len; i += 1) {
+
+      if (chrome.extension.getBackgroundPage()
+          .window.list_config.checkYoutubeSite(tabs[i].url)) {
+        option = document.createElement('option')
+        option.text = tabs[i].title
+        option.value = tabs[i].id
+        select.add(option)
+
+        if (chrome.extension.getBackgroundPage()
+            .window.list_config.getCurrentTab() === option.value) {
+          option.selected = 'selected'
         }
       }
     }
-  });
+  })
 
-  document.getElementById('selectButton').onchange = doSelected;
-});
+  document.getElementById('selectButton').onchange = doSelected
+})
 
-function doSelected(){
-  var select = document.getElementById("selectButton");
-  var tabId = select.options[select.selectedIndex].value;
-  chrome.extension.getBackgroundPage().window.list_config.setCurrentTab(tabId);
+function doSelected() {
+  const select = document.getElementById('selectButton')
+  const tabId = select.options[select.selectedIndex].value
+
+  chrome.extension.getBackgroundPage()
+    .window.list_config.setCurrentTab(tabId)
 }
